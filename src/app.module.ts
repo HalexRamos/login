148171ff051ab2737ea1025e779b589abda8d98a
Halex Ramos <halexramos96@gmail.com>
2023-02-08 11:08:48 -0300

    master Starting project and creating users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
+import { TokensModule } from './tokens/tokens.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    TokensModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
