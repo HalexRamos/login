d05f4868b4ad41a90aa001e85c11e290c087b741
Halex Ramos <halexramos96@gmail.com>
2023-02-27 21:16:50 -0300

    fix: id token uuid

--- src/api/token/entities/token.entity.ts
+++ src/api/token/entities/token.entity.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class Token {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
